253d5d8ba31b6342f4dfc303a6f2d17b0bb05ac5
Thanh Hải Hoàng <haiht27112001@gmail.com>
2021-07-02 23:35:18 +0700

    Added new files

---
+++ js/music-list.js
@@ -0,0 +1,665 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "999 Đóa Hoa Hồng",
+    artist: "",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "BACHATA BASIC",
+    artist: "",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Bài Ca Kỷ Niệm",
+    artist: "Giáng Tiên ft Vũ Duy",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Bài ca người giáo viên nhân dân",
+    artist: "",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Biết đâu nguồn cội",
+    artist: "Tuấn Vũ",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Biển Tình",
+    artist: "Hà Nhi (cover)",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Ca chiu sa",
+    artist: "",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Em yêu anh như câu hò ví dặm",
+    artist: "Anh Thơ",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Cánh Thiệp Đầu Xuân",
+    artist: "Phương Anh",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "CÁT BỤI (ST_ Trịnh Công Sơn)",
+    artist: "Khánh Ly",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Cát Bụi Cuộc Đời (Hà Sơn)",
+    artist: "Đạt Võ",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "CÂU CHUYỆN NHỎ CỦA TÔI",
+    artist: "MIKO LAN TRINH",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "Có phải em mùa thu Hà Nội",
+    artist: "Hồng Nhung",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "Con Đường Chúng Ta Đi(COVER)",
+    artist: "",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "Chân Tình",
+    artist: "Quang Dũng",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "Chỉ Có Bạn Bè Thôi",
+    artist: "Lê Sang ft. Đoàn Minh ft. Tony Tèo",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+    name: "Chiều hạ vàng",
+    artist: "Bảo Yến",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "Chiều Sân Ga",
+    artist: "Quang Lập",
+    img: "music-18",
+    src: "music-18"
+  },
+  {
+    name: "CHỜ ĐÔNG",
+    artist: "MC Phan Phúc Thắng",
+    img: "music-19",
+    src: "music-19"
+  },
+  {
+    name: "Chuyện Ba Mùa Mưa",
+    artist: "Quỳnh Trang",
+    img: "music-20",
+    src: "music-20"
+  },
+  {
+    name: "Chuyến đò quê hương",
+    artist: "Như Hoa cover",
+    img: "music-21",
+    src: "music-21"
+  },
+  {
+    name: "Chuyện Tình Người Đan Áo",
+    artist: "Phương Anh",
+    img: "music-22",
+    src: "music-22"
+  },
+  {
+    name: "Lại Nhớ Người Yêu (Giao Tiên)",
+    artist: "Đan Nguyên",
+    img: "music-23",
+    src: "music-23"
+  },
+  {
+    name: "Đàn sáo Hậu Giang",
+    artist: "Hương Thủy",
+    img: "music-24",
+    src: "music-24"
+  },
+  {
+    name: "Đêm Mưa Nhớ Mẹ",
+    artist: "Hồ Văn Cường",
+    img: "music-25",
+    src: "music-25"
+  },
+  {
+    name: "ĐỘ TA KHÔNG ĐỘ NÀNG",
+    artist: "HƯƠNG LY COVER",
+    img: "music-26",
+    src: "music-26"
+  },
+  {
+    name: "Đôi bờ",
+    artist: "Ngọc Anh",
+    img: "music-27",
+    src: "music-27"
+  },
+  {
+    name: "Em Về Kẻo Trời Mưa",
+    artist: "Dương Hồng Loan ft Lưu Chí Vỹ",
+    img: "music-28",
+    src: "music-28"
+  },
+  {
+    name: "GẶP NHAU GIỮA RỪNG MƠ",
+    artist: "ƯNG LINH",
+    img: "music-29",
+    src: "music-29"
+  },
+  {
+    name: "Gần lắm Trường Sa",
+    artist: "NSƯT Thanh Thúy",
+    img: "music-30",
+    src: "music-30"
+  },
+  {
+    name: "GỬI VỀ QUAN HỌ",
+    artist: "Duy Phường - A Đam",
+    img: "music-31",
+    src: "music-31"
+  },
+  {
+    name: "GIẬN HỜN 2",
+    artist: "NGỌC SƠN FT. HOÀNG CHÂU",
+    img: "music-32",
+    src: "music-32"
+  },
+  {
+    name: "Giận Mà Thương",
+    artist: "Anh Thơ",
+    img: "music-33",
+    src: "music-33"
+  },
+  {
+    name: "Hà Nội Mùa Thu",
+    artist: "Mỹ Linh",
+    img: "music-34",
+    src: "music-34"
+  },
+  {
+    name: "Hà Nội mùa vắng những cơn mưa",
+    artist: "Cẩm Vân",
+    img: "music-35",
+    src: "music-35"
+  },
+  {
+    name: "Hai Mùa Noel",
+    artist: "Phương Anh",
+    img: "music-36",
+    src: "music-36"
+  },
+  {
+    name: "HAI QUÊ",
+    artist: "THANH TÀI FT. BẠCH TRÀ",
+    img: "music-37",
+    src: "music-37"
+  },
+  {
+    name: "HÁT CHÈO - LỜI CHÚC CỦA MÙA XUÂN",
+    artist: "",
+    img: "music-38",
+    src: "music-38"
+  },
+  {
+    name: "NGỌT NGÀO CÂU HÁT MẸ RU ( Đào Liễu)",
+    artist: "NGUYỄN THINH",
+    img: "music-39",
+    src: "music-39"
+  },
+  {
+    name: "Hoa Nở Về Đêm",
+    artist: "Phương Anh",
+    img: "music-40",
+    src: "music-40"
+  },
+  {
+    name: "Hoa Tím Người Xưa",
+    artist: "Dương Hồng Loan",
+    img: "music-41",
+    src: "music-41"
+  },
+  {
+    name: "Buồn Làm Chi Em Ơi",
+    artist: "HOÀI LÂM",
+    img: "music-42",
+    src: "music-42"
+  },
+  {
+    name: "LK Nỗi Buồn Hoa Phượng - Lưu Bút Ngày Xanh (Thanh Sơn)",
+    artist: "Hoàng Oanh_ Hương Lan_ Như Quỳnh",
+    img: "music-43",
+    src: "music-43"
+  },
+  {
+    name: "Hồn Quê",
+    artist: "Hiền Thục",
+    img: "music-44",
+    src: "music-44"
+  },
+  {
+    name: "Kachiusa chế trong quán nhậu cực hài",
+    artist: "",
+    img: "music-45",
+    src: "music-45"
+  },
+  {
+    name: "Khách Đến Chơi Nhà",
+    artist: "Ngọc Ký ft Hương Ly",
+    img: "music-46",
+    src: "music-46"
+  },
+  {
+    name: "Lâu Đài Tình Ái",
+    artist: "Cẩm Ly ft Quốc Đại",
+    img: "music-47",
+    src: "music-47"
+  },
+  {
+    name: "Lk Hành Trình Trên Đất Phù Sa",
+    artist: "Lưu Ánh Loan Ft Dương Hồng Loan",
+    img: "music-48",
+    src: "music-48"
+  },
+  {
+    name: "Lý Cái Mơn",
+    artist: "Ngọc Hân",
+    img: "music-49",
+    src: "music-49"
+  },
+  {
+    name: "Lý Chiều Chiều",
+    artist: "Thu Hiền",
+    img: "music-50",
+    src: "music-50"
+  },
+  {
+    name: "Lý chim xanh",
+    artist: "Dương Hồng Loan",
+    img: "music-51",
+    src: "music-51"
+  },
+  {
+    name: "Bèo dạt mây trôi",
+    artist: "Anh Khang FT. Tạ Quang Thắng",
+    img: "music-52",
+    src: "music-52"
+  },
+  {
+    name: "Miền Tây Quê Tôi",
+    artist: "Dương Đình Trí ft Thanh Ngân",
+    img: "music-53",
+    src: "music-53"
+  },
+  {
+    name: "Đã Không Yêu Thì Thôi (Hoài An)",
+    artist: "Minh Tuyết",
+    img: "music-54",
+    src: "music-54"
+  },
+  {
+    name: "Một Đời Người Một Rừng Cây",
+    artist: "Trọng Tấn",
+    img: "music-55",
+    src: "music-55"
+  },
+  {
+    name: "MỘNG ĐẸP NGÀY XƯA",
+    artist: "",
+    img: "music-56",
+    src: "music-56"
+  },
+  {
+    name: "Một Chuyến Bay Đêm",
+    artist: "Thúy Hà",
+    img: "music-57",
+    src: "music-57"
+  },
+  {
+    name: "Một Lần Dang Dở",
+    artist: "Quang Lê",
+    img: "music-58",
+    src: "music-58"
+  },
+  {
+    name: "Mưa chiều miền Trung",
+    artist: "Nguyễn Quốc Linh",
+    img: "music-59",
+    src: "music-59"
+  },
+  {
+    name: "Mưa Rừng",
+    artist: "Dương Hồng Loan",
+    img: "music-60",
+    src: "music-60"
+  },
+  {
+    name: "NẾU ANH ĐỪNG HẸN",
+    artist: "TIẾN VƯƠNG",
+    img: "music-61",
+    src: "music-61"
+  },
+  {
+    name: "NGHE CÂU QUAN HỌ TRÊN CAO NGUYÊN",
+    artist: "DUY PHƯỜNG - HỒNG NHUNG",
+    img: "music-62",
+    src: "music-62"
+  },
+  {
+    name: "Ngõ Vắng Xôn Xao (Trần Quang Huy)",
+    artist: "Quang Dũng",
+    img: "music-63",
+    src: "music-63"
+  },
+  {
+    name: "Người Tình Không Đến",
+    artist: "Quỳnh Trang",
+    img: "music-64",
+    src: "music-64"
+  },
+  {
+    name: "Nhớ mùa thu Hà Nội",
+    artist: "Hồng Nhung",
+    img: "music-65",
+    src: "music-65"
+  },
+  {
+    name: "Nhớ về Hà Nội",
+    artist: "Hồng Nhung",
+    img: "music-66",
+    src: "music-66"
+  },
+  {
+    name: "NHỚ VỀ HỘI LIM",
+    artist: "TƯỜNG VI",
+    img: "music-67",
+    src: "music-67"
+  },
+  {
+    name: "Duyên Phận (Thái Thịnh)",
+    artist: "Như Quỳnh",
+    img: "music-68",
+    src: "music-68"
+  },
+  {
+    name: "Em Đi Xem Hội Trăng Rằm (Nguyễn Nghị)",
+    artist: "Như Quỳnh",
+    img: "music-69",
+    src: "music-69"
+  },
+  {
+    name: "Người Ngoài Phố (Anh Việt Thu)",
+    artist: "Như Quỳnh",
+    img: "music-70",
+    src: "music-70"
+  },
+  {
+    name: "Trả Lại Thời Gian (Thanh Sơn)",
+    artist: "Như Quỳnh",
+    img: "music-71",
+    src: "music-71"
+  },
+  {
+    name: "Vùng Lá Me Bay (Anh Việt Thanh)",
+    artist: "Như Quỳnh",
+    img: "music-72",
+    src: "music-72"
+  },
+  {
+    name: "Bến Thượng Hải",
+    artist: "Như Quỳnh FT. Nguyễn Hưng",
+    img: "music-73",
+    src: "music-73"
+  },
+  {
+    name: "LK Con Đường Xưa Em Đi - Xin Anh Giữ Trọn Tình Quê",
+    artist: "Như Quỳnh FT. Trường Vũ",
+    img: "music-74",
+    src: "music-74"
+  },
+  {
+    name: "Ơn Nghĩa Sinh Thành",
+    artist: "Trường Vũ",
+    img: "music-75",
+    src: "music-75"
+  },
+  {
+    name: "Hương Tóc Mạ Non",
+    artist: "Quang Lê FT. Hà Phương",
+    img: "music-76",
+    src: "music-76"
+  },
+  {
+    name: "Chiều Lên Bản Thượng (Lê Dinh)",
+    artist: "Phi Nhung",
+    img: "music-77",
+    src: "music-77"
+  },
+  {
+    name: "Chiều Qua Phà Hậu Giang (Trịnh Lâm Ngân)",
+    artist: "Phi Nhung",
+    img: "music-78",
+    src: "music-78"
+  },
+  {
+    name: "Phải Lòng Con Gái Bến Tre (Phan Ni Tấn_ thơ_ Luân Hoán)",
+    artist: "Phi Nhung",
+    img: "music-79",
+    src: "music-79"
+  },
+  {
+    name: "Đón Xuân Này Nhớ Xuân Xưa (Châu Kỳ_ Mạnh Quỳnh)",
+    artist: "Phi Nhung FT. Mạnh Quỳnh",
+    img: "music-80",
+    src: "music-80"
+  },
+  {
+    name: "Quảng Bình quê ta ơi",
+    artist: "Phương Nga",
+    img: "music-81",
+    src: "music-81"
+  },
+  {
+    name: "Cõi Nhớ",
+    artist: "Quang Lê",
+    img: "music-82",
+    src: "music-82"
+  },
+  {
+    name: "Tâm Sự Đời Tôi",
+    artist: "Dương Hồng Loan",
+    img: "music-83",
+    src: "music-83"
+  },
+  {
+    name: "Tây Vương Nữ Quốc",
+    artist: "",
+    img: "music-84",
+    src: "music-84"
+  },
+  {
+    name: "TÌM EM QUA CÂU DÂN CA",
+    artist: "DUY PHƯỜNG FT. TƯỜNG VI",
+    img: "music-85",
+    src: "music-85"
+  },
+  {
+    name: "TÌNH ANH",
+    artist: "HƯƠNG LY COVER",
+    img: "music-86",
+    src: "music-86"
+  },
+  {
+    name: "TÌNH ANH",
+    artist: "ĐÌNH DŨNG",
+    img: "music-87",
+    src: "music-87"
+  },
+  {
+    name: "Tình Ấm Chiều Quê",
+    artist: "Khưu Huy Vũ ft Dương Hồng Loan",
+    img: "music-88",
+    src: "music-88"
+  },
+  {
+    name: "Tình Cây Và Đất",
+    artist: "Anh Thơ",
+    img: "music-89",
+    src: "music-89"
+  },
+  {
+    name: "Tình lỡ cách xa",
+    artist: "Mỹ Tâm",
+    img: "music-90",
+    src: "music-90"
+  },
+  {
+    name: "TÌNH NGHÈO CÓ NHAU",
+    artist: "Quỳnh Trang Ft Thiên Quang",
+    img: "music-91",
+    src: "music-91"
+  },
+  {
+    name: "TÌNH NHI NỮ",
+    artist: "DIỄM SON",
+    img: "music-92",
+    src: "music-92"
+  },
+  {
+    name: "Tơ Duyên",
+    artist: "Huỳnh Đông ft Vân Trang",
+    img: "music-93",
+    src: "music-93"
+  },
+  {
+    name: "Thánh Ca Lễ Cưới - Diễm Tình Ca 3",
+    artist: "Ái Trinh",
+    img: "music-94",
+    src: "music-94"
+  },
+  {
+    name: "Thề Non Hẹn Biển",
+    artist: "Trường Sơn FT. Kim Thư",
+    img: "music-95",
+    src: "music-95"
+  },
+  {
+    name: "THƠ TÌNH CUỐI MÙA THU",
+    artist: "THU UYÊN",
+    img: "music-96",
+    src: "music-96"
+  },
+  {
+    name: "Bác Hồ Một Tình Yêu Bao La",
+    artist: "Thu Hiền",
+    img: "music-97",
+    src: "music-97"
+  },
+  {
+    name: "Thuyền Hoa",
+    artist: "Quang Linh ft Hà Phương",
+    img: "music-98",
+    src: "music-98"
+  },
+  {
+    name: "Thương Lắm Miền Trung Ơi",
+    artist: "Phương Anh",
+    img: "music-99",
+    src: "music-99"
+  },
+  {
+    name: "Vào Chùa",
+    artist: "NSND Thúy Hường",
+    img: "music-100",
+    src: "music-100"
+  },
+  {
+    name: "Về Đâu Mái Tóc Người Thương",
+    artist: "Quang Lê",
+    img: "music-101",
+    src: "music-101"
+  },
+  {
+    name: "Về Miền Tây",
+    artist: "Mai Lệ Quyên",
+    img: "music-102",
+    src: "music-102"
+  },
+  {
+    name: "Vũ Điệu ChaChaCha",
+    artist: "",
+    img: "music-103",
+    src: "music-103"
+  },
+  {
+    name: "Vu Lan Nhớ Mẹ",
+    artist: "",
+    img: "music-104",
+    src: "music-104"
+  },
+  {
+    name: "VÀO HẠ",
+    artist: "Hoà Minzy x Anh Tú x Hứa Kim Tuyền",
+    img: "music-105",
+    src: "music-105"
+  },
+  {
+    name: "Xuân Yêu Thương Remix",
+    artist: "Đàm Vĩnh Hưng",
+    img: "music-106",
+    src: "music-106"
+  },
+  {
+    name: "Yêu Một Mình",
+    artist: "Y Phụng",
+    img: "music-107",
+    src: "music-107"
+  },
+  
+  
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+  
+];
